249eab866890caee7998867410a68382e1f0506f
sojib <sojib.ali@hotmail.com>
2025-07-01 20:46:16 +0600

    tested dynamically set an HTTP header on the response

--- hello.py
+++ hello.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI, Header
+
+
+from fastapi import FastAPI, Header, Response
 
 app = FastAPI()
 
 @app.post("/hi")
 def greet(who: str = Header("World", alias="X-Who")):
     return f"ola {who}"
+
+@app.get("/happy")
+def happy(status_code=200):
+    return ":)"
+
+@app.get("/header/{name}/{value}")
+def set_response_header(name: str, value: str, response: Response):
+    response.headers[name] = value
+    return "normal body"
+
+
 
 if __name__ == "__main__":
     import uvicorn
