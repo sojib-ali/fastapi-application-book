c5ace0c268e11dfe5c0ac3c2692ed8b28a689fc9
sojib <sojib.ali@hotmail.com>
2025-07-19 17:38:11 +0600

    added model and fake data to web and connect it to main

---
+++ src/fake/creature.py
@@ -0,0 +1,39 @@
+from ..model.creature import Creature
+
+_creatures = [
+    Creature(name="Yeti", 
+             aka="Abominable Snowman", 
+             country="CN", 
+             area="Himalayas", 
+             description="Hirsute Himalayan"),
+
+    Creature(name="Bigfoot", 
+             description="Yeti's Cousin Eddie", 
+             country="US", 
+             area="*", 
+             aka="Sasquatch")
+]
+
+def get_all() -> list[Creature]:
+    #return all creatures
+    return _creatures
+
+def get_one(name: str) -> Creature | None:
+    #return one creature
+    for _creature in _creatures:
+        if _creature.name == name:
+            return _creature
+    return None
+
+def create(creature: Creature) -> Creature: 
+    """Add a creature""" 
+    return creature
+def modify(creature: Creature) -> Creature: 
+    """Partially modify a creature""" 
+    return creature
+def replace(creature: Creature) -> Creature: 
+    """Completely replace a creature""" 
+    return creature
+def delete(name: str):
+#Delete a creature: return None if it existed 
+    return None
---
+++ src/fake/explorer.py
@@ -0,0 +1,36 @@
+from ..model.explorer import Explorer
+
+_explorers = [
+    Explorer(name = "Claude Hande",
+             country = "FR",
+             description = "Scarce during full moons"),
+    Explorer(name = "Noah Weiser",
+             country = "DE",
+             description = "Myopic machete man"),
+]
+
+def get_all() -> list[Explorer]:
+    #return all explorers
+    return _explorers
+
+def get_one(name: str) -> Explorer | None: 
+    for _expolorer in _explorers:
+        if _expolorer.name == name:
+            return _expolorer
+    return None
+
+def create(explorer:Explorer) -> Explorer:
+    #Add an explorer
+    return explorer
+
+def modify(explorer: Explorer) -> Explorer:
+    #partially modify an explorer
+    return explorer
+
+def replace(explorer: Explorer) -> Explorer:
+    #completely replace an explorer
+    return explorer
+
+def delete(name: str) -> bool: 
+    #Delete an explorer: return None if it existed
+    return None
